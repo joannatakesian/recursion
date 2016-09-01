f13827e4a9c0ae9ad91f81e97a09d761f89a3afe
Joanna Takesian <joanna.takesian@gmail.com>
2016-08-31 21:29:22 -0700

    refactored merge sort

--- merge_sort.rb
+++ merge_sort.rb
@@ -1,29 +1,27 @@
 def merge_sort(list)
     left, right, sorted_list = []
     
-    if list.size <= 1
-        return list
-    else    
-        midpoint = list.size/2
-        left = list[0...midpoint]
-        right = list[midpoint..-1]
-        left = merge_sort(left)
-        right = merge_sort(right)
+    return list if list.size <= 1
 
-        if left[-1] <= right [0]
-            left.concat right
-            return left
-        end
+    midpoint = list.size/2
+    left = list[0...midpoint]
+    right = list[midpoint..-1]
+    left = merge_sort(left)
+    right = merge_sort(right)
 
-        sorted_list = merge(left, right)
-        return sorted_list
+    if left[-1] <= right [0]
+        left.concat right
+        return left
     end
+
+    sorted_list = merge(left, right)
+    return sorted_list
 end
 
 def merge(left, right)
     sorted_list = []
     
-    while left.size > 0 && right.size > 0
+    while left.size > 0 && right.size > 0 do
         if left[0] <= right[0]
             sorted_list << left.shift
         else
@@ -31,13 +29,8 @@ def merge(left, right)
         end
     end
     
-    if left.size > 0
-        sorted_list.concat left
-    end
-        
-    if right.size > 0
-        sorted_list.concat right
-    end
+    sorted_list.concat left if left.size > 0
+    sorted_list.concat right if right.size > 0
 
     return sorted_list
 
